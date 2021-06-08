0c6ad9ee6c8640652a6db9215bfe2ada4651ab3f
nuttertools <m.nuttasuk@gmail.com>
2021-06-08 10:53:44 +0700

    Complete basic React

---
+++ src/Board.js
@@ -0,0 +1,105 @@
+import React, { Component } from "react";
+import Cell from "./Cell";
+
+const validate = board => {
+  let isValid = true;
+  for (let i = 0; i < 4; i++) {
+    const horizontal = new Set();
+    const vertical = new Set();
+    const square = new Set();
+    for (let j = 0; j < 4; j++) {
+      horizontal.add(board[i][j]);
+      vertical.add(board[j][i]);
+      square.add(
+        board[2 * (i % 2) + (j % 2)][2 * Math.floor(i / 2) + Math.floor(j / 2)],
+      );
+    }
+    horizontal.delete(0);
+    vertical.delete(0);
+    square.delete(0);
+    if (horizontal.size !== 4 || vertical.size !== 4 || square.size !== 4) {
+      isValid = false;
+    }
+  }
+  return isValid;
+};
+
+class Board extends Component {
+  state = {
+    loading: true,
+    statusText: "",
+    timer: 0,
+  };
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({ timer: this.state.timer + 1 });
+    }, 1000);
+    this.restartBoard();
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  submit = () => {
+    const isValid = validate(this.state.board);
+    if (isValid) {
+      clearInterval(this.interval);
+    }
+    this.setState({
+      statusText: isValid ? "Board is complete" : "Board is invalid ",
+    });
+  };
+
+  restartBoard = () => {
+    this.setState({
+      loading: true,
+    });
+    fetch(
+      "https://us-central1-skooldio-courses.cloudfunctions.net/react_01/random",
+    )
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          loading: false,
+          board: data.board,
+          timer: 0,
+          initial: data.board.map(row => row.map(item => item !== 0)),
+          statusText: "",
+        });
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <p className="timer">Elapsed Time: {this.state.timer} seconds</p>
+        <div className="board">
+          {!this.state.loading &&
+            this.state.board.map((row, i) =>
+              row.map((number, j) => (
+                <Cell
+                  isInitial={this.state.initial[i][j]}
+                  key={`cell-${i}-${j}`}
+                  number={number}
+                  onChange={newNumber => {
+                    const { board } = this.state;
+                    board[i][j] = newNumber;
+                    this.setState({ board });
+                  }}
+                ></Cell>
+              )),
+            )}
+        </div>
+        <button className="restart-button" onClick={this.restartBoard}>
+          Restart
+        </button>
+        <button onClick={this.submit}>Submit</button>
+        <p>{this.state.statusText}</p>
+      </div>
+    );
+  }
+}
+
+export default Board;
---
+++ src/Cell.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Cell = ({ isInitial, number, onChange }) => (
+  <div
+    onClick={e => {
+      if (isInitial) return;
+      onChange((number + 1) % 5);
+    }}
+    className={`cell ${isInitial ? "initial" : ""}`}
+  >
+    {number !== 0 && number}
+  </div>
+);
+
+export default Cell;
